8d7d64c23fa35510e5c270c8b067ec5fe086a2c9
davidmonteiro03 <damonteiro12446@gmail.com>
2025-02-16 11:19:45 +0000

    git updates

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,15 +6,15 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 19:17:30 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/09 19:18:16 by dcaetano         ###   ########.fr       */
+/*   Updated: 2025/02/16 11:14:25 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <unistd.h>
 # include <stdarg.h>
+# include <unistd.h>
 
 int	ft_printf(const char *format, ...);
 
